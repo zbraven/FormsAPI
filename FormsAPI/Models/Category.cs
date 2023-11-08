3052a942149949092bf85207db788c4931fc7b9c
Zahid Buğra Sarıcan <zbsarican@gmail.com>
2023-11-08 11:42:59 +0300

    Add project files.

---
+++ FormsAPI/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace FormsAPI.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
---
+++ FormsAPI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace FormsAPI.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
