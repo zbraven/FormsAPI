e7a6b5fce066000fa1033818899da7db212d62c5
Zahid Buğra Sarıcan <zbsarican@gmail.com>
2023-11-08 17:01:56 +0300

    ProductViewModel olarak çağırıyorum artık modelimi

---
+++ FormsAPI/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FormsAPI.Models
+{
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; } = null;
+        public List<Category> Categories { get; set; } = null;
+
+        public string SelectedCategory { get; set; }
+
+    }
+}
