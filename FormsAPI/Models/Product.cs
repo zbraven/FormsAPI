5aa05abb140492455bbe5288554c004dcae49623
Zahid Buğra Sarıcan <zbsarican@gmail.com>
2023-11-08 19:40:38 +0300

    Form Tag Helpers

--- FormsAPI/Models/Product.cs
+++ FormsAPI/Models/Product.cs
@@ -6,13 +6,17 @@ namespace FormsAPI.Models
     {
         [Display(Name="Ürün ID")]
         public int ProductId { get; set; }
+
         [Display(Name = "Ürün Adı")]
+        [Required]
         public string Name { get; set; } = string.Empty;
         [Display(Name = "Fiyat")]
         public decimal Price { get; set; }
         [Display(Name = "Resim")]
         public string Image { get; set; } = string.Empty;
         public bool IsActive { get; set; }
+
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
     }
 }
